bc57ca82c00635b693447b3f5dfea1cd7a8e507b
noanoa07 <aqua055@gmail.com>
2016-01-18 23:52:47 +0900

    update for ev3dev-jessie-2015-12-30

--- lib/ev3dev/sensor.rb
+++ lib/ev3dev/sensor.rb
@@ -4,7 +4,7 @@ module Ev3dev
 
     def initialize(port)
       Dir.glob("#{PATH}/sensor*").each do |path|
-        if IO.read("#{path}/port_name").strip == "in#{port.to_s}"
+        if IO.read("#{path}/address").strip == "in#{port.to_s}"
           super path
           return
         end
